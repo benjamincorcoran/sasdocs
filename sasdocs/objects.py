34b8ad8168be978b0ac22dbfa10ffecf7cbd93c0
Ben <ben.corcoran@officeforstudents.org.uk>
2019-12-06 16:08:47 +0000

    add macro parsing, re-order objects.py

--- sasdocs/objects.py
+++ sasdocs/objects.py
@@ -4,6 +4,32 @@ import attr
 import re
 import parsy as ps
 
+def force_partial_parse(parser, string, **kwargs):
+    """Force partial parse of string skipping unparsable characters
+    
+    Parameters:
+    parser (parsy.parser): parsy valid parsing object
+    string (str): String to be parsed
+
+    Returns:
+    list: parsed objects from string"""
+    if isinstance(string, str):
+        parsed = []
+        olen = len(string)
+        skips = 0
+        while len(string) > 0:
+            partialParse, string = parser.parse_partial(string)
+            if len(partialParse) == 0:
+                string = string[1:]
+                skips += 1
+            else:
+                parsed += partialParse
+        
+        # print("Parsed: {:.2%}".format(1-(skips/olen)))
+        return [p for p in parsed if p != '\n']
+    else:
+        return []
+
 # SAS object classes 
 # Class objects are roughly equivalent to abstracted SAS concepts
 # as such they are 
@@ -15,48 +41,6 @@ import parsy as ps
 #     - procedure
 #     - libname
 #     - include
-
-@attr.s
-class macroVariable:
-    variable = attr.ib()
-
-@attr.s
-class macroVariableDefinition:
-    variable = attr.ib()
-    value = attr.ib()
-
-@attr.s
-class comment:
-    text = attr.ib()
-
-@attr.s
-class dataObject:
-    library = attr.ib()
-    dataset = attr.ib()
-    options = attr.ib(default=None)
-
-@attr.s
-class dataStep:
-    outputs = attr.ib()
-    header = attr.ib()
-    inputs = attr.ib()
-    body = attr.ib()
-
-@attr.s
-class procedure:
-    outputs = attr.ib()
-    inputs = attr.ib()
-    type = attr.ib()
-    
-@attr.s 
-class libname:
-    library = attr.ib()
-    path = attr.ib()
-    pointer = attr.ib(default=None)
-
-@attr.s 
-class include:
-    path = attr.ib()
 
 
 # Parsy Objects
@@ -88,6 +72,7 @@ amp = ps.string('&')
 lb = ps.string('(')
 rb = ps.string(')')
 star = ps.string('*')
+cmm = ps.string(',')
 
 # Multiline comment entry and exit points
 comstart = ps.string(r'/*')
@@ -97,19 +82,27 @@ comend = ps.string(r'*/')
 qte = ps.string('"') | ps.string("'")
 
 # Common SAS keywords
-run = ps.regex('run',flags=re.IGNORECASE)
-qt = ps.regex('quit',flags=re.IGNORECASE)
+run = ps.regex(r'run',flags=re.IGNORECASE)
+qt = ps.regex(r'quit',flags=re.IGNORECASE)
 
 
 # Basic SAS Objects
 
 # Macrovariable: ampersand + word + optional dot
+@attr.s
+class macroVariable:
+    variable = attr.ib()
+
 mcv = (amp + wrd + opdot).map(macroVariable)
 
+@attr.s
+class comment:
+    text = attr.ib()
+
 # Inline comment: start + commentry + semicolon
-inlinecmnt = star >> ps.regex('[^;]+') << col
+inlinecmnt = star >> ps.regex(r'[^;]+') << col
 # Multiline comment: Commentary start + commentry + Commentry end
-multicmnt = comstart >> ps.regex('.+?(?=\*\/)', flags=reFlags) << comend
+multicmnt = comstart >> ps.regex(r'.+?(?=\*\/)', flags=reFlags) << comend
 
 # Either inline or multiline comment, mapped to comment object
 cmnt = (inlinecmnt|multicmnt).map(comment)
@@ -119,9 +112,14 @@ cmnt = (inlinecmnt|multicmnt).map(comment)
 sasName = (wrd|mcv).many()
 
 # Marcovariable definition:
+@attr.s
+class macroVariableDefinition:
+    variable = attr.ib()
+    value = attr.ib()
+
 mcvDef = ps.seq(
-    variable =ps.regex('%let',flags=reFlags) + spc + opspc >> sasName << opspc + eq,
-    value = ps.regex('[^;]+', flags=reFlags).optional(),
+    variable =ps.regex(r'%let',flags=reFlags) + spc + opspc >> sasName << opspc + eq,
+    value = ps.regex(r'[^;]+', flags=reFlags).optional(),
     _col = col
 ).combine_dict(macroVariableDefinition)
 
@@ -129,7 +127,7 @@ mcvDef = ps.seq(
 # e.g. where=(1=1)
 datalineArg = ps.seq(
     option =sasName << (opspc + eq + opspc), 
-    setting = lb + ps.regex('[^)]*') + rb
+    setting = lb + ps.regex(r'[^)]*') + rb
 )
 
 # datalineOptions: Seperate multiple datalineArgs by spaces
@@ -141,6 +139,13 @@ datalineOptions = ps.seq(
 #   - library: sasName before . if . exists
 #   - dataset: sasName after . or just sasName required
 #   - options: dataLineOptions is present
+
+@attr.s
+class dataObject:
+    library = attr.ib()
+    dataset = attr.ib()
+    options = attr.ib(default=None)
+
 dataObj = ps.seq(
     library = (sasName << dot).optional(),
     dataset = dot >> sasName | sasName,
@@ -156,11 +161,19 @@ dataLine = dataObj.sep_by(spc)
 #   - inputs: dataline directly after set/merge statement
 #   - body: anything between inputs and "run"
 # terminating run is thrown away
+
+@attr.s
+class dataStep:
+    outputs = attr.ib()
+    header = attr.ib()
+    inputs = attr.ib()
+    body = attr.ib()
+
 datastep = ps.seq(
-    outputs = (ps.regex('data', flags=re.IGNORECASE) + spc) >> dataLine << col,
-    header = ps.regex('.*?(?=set|merge)', flags=reFlags),
-    inputs = (opspc + ps.regex('set|merge',flags=re.IGNORECASE) + opspc) >> dataLine << col,
-    body = ps.regex('.*?(?=run)', flags=reFlags),
+    outputs = (ps.regex(r'data', flags=re.IGNORECASE) + spc) >> dataLine << col,
+    header = ps.regex(r'.*?(?=set|merge)', flags=reFlags),
+    inputs = (opspc + ps.regex(r'set|merge',flags=re.IGNORECASE) + opspc) >> dataLine << col,
+    body = ps.regex(r'.*?(?=run)', flags=reFlags),
     _run = run + opspc + col
 ).combine_dict(dataStep)
 
@@ -169,13 +182,20 @@ datastep = ps.seq(
 #   - inputs: capture single dataObj after "data="
 #   - outputs: capture single dataObj after "out="
 # throw away an other excess
+@attr.s
+class procedure:
+    outputs = attr.ib()
+    inputs = attr.ib()
+    type = attr.ib()
+    
+
 proc = ps.seq(
-    type = (ps.regex('proc', flags=re.IGNORECASE) + spc) >> wrd << spc,
-    _h1 = ps.regex('.*?(?=data)', flags=reFlags),
-    inputs = (ps.regex('data', flags=re.IGNORECASE) + opspc + eq + opspc) >> dataObj,
-    _h2 = ps.regex('.*?(?=out\s*=)', flags=reFlags).optional(),
-    outputs = ((ps.regex('out', flags=re.IGNORECASE) + opspc + eq + opspc) >> dataObj).optional(),
-    _h3 = ps.regex('.*?(?=run|quit)', flags=reFlags),
+    type = (ps.regex(r'proc', flags=re.IGNORECASE) + spc) >> wrd << spc,
+    _h1 = ps.regex(r'.*?(?=data)', flags=reFlags),
+    inputs = (ps.regex(r'data', flags=re.IGNORECASE) + opspc + eq + opspc) >> dataObj,
+    _h2 = ps.regex(r'.*?(?=out\s*=)', flags=reFlags).optional(),
+    outputs = ((ps.regex(r'out', flags=re.IGNORECASE) + opspc + eq + opspc) >> dataObj).optional(),
+    _h3 = ps.regex(r'.*?(?=run|quit)', flags=reFlags),
     _run = (run|qt) + opspc + col
 ).combine_dict(procedure)
 
@@ -183,6 +203,14 @@ proc = ps.seq(
 #   - library: name of library reference in code 
 #   - path: filepath of the library if given 
 #   - pointer: name of library reference pointed to if given
+
+@attr.s 
+class libname:
+    library = attr.ib()
+    path = attr.ib()
+    pointer = attr.ib(default=None)
+
+
 lbnm = ps.seq(
     library = (ps.regex(r'libname', flags=re.IGNORECASE) + spc) >> sasName << spc,
     path = (opspc + qte >> fpth << opspc + qte + col).optional(),
@@ -191,33 +219,48 @@ lbnm = ps.seq(
 
 # icld: Abstracted include statement, one component:
 #   - path: file path to the included code 
+@attr.s 
+class include:
+    path = attr.ib()
+
 icld = ps.seq(
     path = (ps.regex(r'%include', flags=re.IGNORECASE) + spc + opspc + qte) >> fpth << (qte + opspc + col),
 ).combine_dict(include)
 
-
+# program: Multiple SAS objects in any order
 program = (nl|mcvDef|cmnt|datastep|proc|lbnm|icld).many()
 
 
-def force_partial_parse(parser, string, **kwargs):
-    """Force partial parse of string skipping unparsable characters
-    
-    Parameters:
-    parser (parsy.parser): parsy valid parsing object
-    string (str): String to be parsed
+@attr.s
+class macroargument:
+    arg = attr.ib()
+    default = attr.ib()
+    doc = attr.ib()
 
-    Returns:
-    list: parsed objects from string"""
-    parsed = []
-    olen = len(string)
-    skips = 0
-    while len(string) > 0:
-        partialParse, string = parser.parse_partial(string)
-        if len(partialParse) == 0:
-            string = string[1:]
-            skips += 1
-        else:
-            parsed += partialParse
-    
-    # print("Parsed: {:.2%}".format(1-(skips/olen)))
-    return parsed
+@attr.s
+class macro:
+    name = attr.ib()
+    arguments = attr.ib()
+    body = attr.ib()
+
+    def __attrs_post_init__(self):
+        if isinstance(self.body,str):
+            self.body = force_partial_parse(program,self.body)
+
+mcroarg = ps.seq(
+    arg = sasName << opspc,
+    default = (eq + opspc >> sasName).optional(),
+    doc = cmnt.optional()
+).combine_dict(macroargument)
+
+mcroargline = lb + opspc >> mcroarg.sep_by(opspc+cmm+opspc) << opspc + rb
+
+mcro = ps.seq(
+    name = ps.regex(r'%macro',flags=re.IGNORECASE) + spc + opspc >> sasName,
+    arguments = (opspc >> mcroargline).optional(),
+    body = opspc + col >> ps.regex(r'.*?(?=%mend)',flags=reFlags) << ps.regex(r'%mend',flags=re.IGNORECASE) + opspc + col
+).combine_dict(macro)
+
+# fullprogram: multiple SAS objects including macros
+fullprogram = (nl|mcvDef|cmnt|datastep|proc|lbnm|icld|mcro).many()
+
--- tests/test_program.py
+++ tests/test_program.py
@@ -1,8 +1,8 @@
 import pytest
 import pprint
-from sasdocs.objects import program, force_partial_parse
+from sasdocs.objects import *
 
-sasCode = """
+testcases = [("""
 libname a "path/to/folder";
 %let a = 1;
 data a.data2;
@@ -19,8 +19,58 @@ run;
 proc transpose data=a.data3 out=a.data4;
     by a;
 run;
-"""
-parsed = force_partial_parse(program, sasCode)
-parsed1 = [item for item in parsed if item != '\n']
-print(parsed)
-pprint.pprint(parsed1)
+""",
+[
+libname(library=['a'], path='path/to/folder', pointer=None),
+macroVariableDefinition(variable=['a'], value=' 1'),
+dataStep(outputs=[dataObject(library=['a'], dataset=['data2'], options=None)], header='\n    ', inputs=[dataObject(library=['a'], dataset=['data1'], options=None)], body='\n'),
+comment(text='inline comment'),
+comment(text='inline comment'),
+comment(text='Comment'),
+procedure(outputs=dataObject(library=['a'], dataset=['data3'], options=None), inputs=dataObject(library=['a'], dataset=['data2'], options=None), type='summary'),
+procedure(outputs=dataObject(library=['a'], dataset=['data4'], options=None), inputs=dataObject(library=['a'], dataset=['data3'], options=None), type='transpose')
+])]
+
+@pytest.mark.parametrize("case,expected", testcases)
+def test_force_partial_parse(case, expected):
+    res = force_partial_parse(program, case)
+    res = [item for item in res if item != '\n']
+    assert res == expected
+
+
+testcases = [("""
+libname a "path/to/folder";
+%let a = 1;
+data a.data2;
+    set a.data1;
+run;
+%macro test;
+*inline comment;
+*inline comment;
+/*Comment*/
+proc summary data=a.data2 nway; 
+    class a b c;
+    output out=a.data3;
+run;
+%mend;
+proc transpose data=a.data3 out=a.data4;
+    by a;
+run;
+""",
+[
+libname(library=['a'], path='path/to/folder', pointer=None),
+macroVariableDefinition(variable=['a'], value=' 1'),
+dataStep(outputs=[dataObject(library=['a'], dataset=['data2'], options=None)], header='\n    ', inputs=[dataObject(library=['a'], dataset=['data1'], options=None)], body='\n'),
+macro(name=['test'], arguments=None, body=[
+    comment(text='inline comment'),
+    comment(text='inline comment'),
+    comment(text='Comment'),
+    procedure(outputs=dataObject(library=['a'], dataset=['data3'], options=None), inputs=dataObject(library=['a'], dataset=['data2'], options=None), type='summary')
+]),
+procedure(outputs=dataObject(library=['a'], dataset=['data4'], options=None), inputs=dataObject(library=['a'], dataset=['data3'], options=None), type='transpose')
+])]
+
+@pytest.mark.parametrize("case,expected", testcases)
+def test_force_partial_marco_parse(case, expected):
+    res = force_partial_parse(fullprogram, case)
+    assert res == expected
