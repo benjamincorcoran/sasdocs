81ee5a9c8f603a8a13e612c404716e168f671bee
Ben Corcoran <benjamin.corcoran@gmail.com>
2019-12-28 13:11:23 +0000

    Update project template for production of markdown docs

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,11 +1,23 @@
 import pytest
 import pprint
+import jinja2 as ji
 
 from sasdocs.project import sasProject
 
 
+def write_to_markdown(project):
+    with open('./sasdocs/templates/project.tplt', 'r') as f:
+        tmplt = f.read()
 
-# for x in prj.get_objects():
-#     print(x)
+    t = ji.Template(tmplt)
 
-# Include tests
+    with open('./tests/samples/output.md', 'w') as f:
+        f.write(t.render(project=test_project))
+
+
+
+test_project = sasProject(r'./tests/samples/')
+for x in test_project.get_objects(objectType='macro'):
+    print(x)
+
+write_to_markdown(test_project)
