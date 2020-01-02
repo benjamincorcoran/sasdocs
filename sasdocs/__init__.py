864bb226f8ca894cbecc58690fe4d182634596f8
Ben Corcoran <Ben.Corcoran@officeforstudents.org.uk>
2020-01-02 11:39:55 +0000

    Update format_logger to remove duplicate streams + add fileout

--- sasdocs/__init__.py
+++ sasdocs/__init__.py
@@ -1,11 +1,16 @@
 import logging
 
-def format_logger(logger, context):
-
-    logger_stream = logging.StreamHandler()
-    logger_fmt = logging.Formatter(r'%(levelname)s:%(name)s: "%(path)s": %(message)s')
-    logger_stream.setFormatter(logger_fmt)
-    logger.setLevel(logging.INFO)
-    logger.addHandler(logger_stream)
+def format_logger(logger, context, logOut='sasdocs.log'):
+    if not logger.handlers:
+        logger_stream = logging.StreamHandler()
+        logger_fmt = logging.Formatter(r'%(levelname)s:%(name)s: "%(path)s": %(message)s')
+        logger_stream.setFormatter(logger_fmt)
+        logger.setLevel(logging.INFO)
+        logger.addHandler(logger_stream)
+            
+        logger_file = logging.FileHandler(logOut)
+        logger_file.setLevel(logging.DEBUG)
+        logger_file.setFormatter(logger_fmt)
+        logger.addHandler(logger_file)
 
     return logging.LoggerAdapter(logger, context)
