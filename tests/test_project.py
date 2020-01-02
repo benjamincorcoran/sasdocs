bde7caf79efb696c97ea78f31a95a8c2026c814f
Ben Corcoran <Ben.Corcoran@officeforstudents.org.uk>
2020-01-02 11:39:22 +0000

    testing

--- tests/test_project.py
+++ tests/test_project.py
@@ -4,6 +4,6 @@ import pprint
 
 from sasdocs.project import sasProject
 
-test_project = sasProject(r'./tests/samples/')
-test_project.generate_documentation(outputDirectory='docs')
+test_project = sasProject(r'S:\HESA AP Student\2017-18\Data checking tool\Code\Live')
+test_project.generate_documentation(outputDirectory='W:\docs')
 
