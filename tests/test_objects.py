831dd1bdfb2e26f3ca44731f9c0e8bf523af98bd
Ben Corcoran <Ben.Corcoran@officeforstudents.org.uk>
2020-01-02 17:25:25 +0000

    Handle malformed macro better, fixes  #19

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -237,3 +237,39 @@ def test_force_partial_marco_parse(case, expected):
     assert res == expected
 
 
+testcases = [("""
+libname a "path/to/folder";
+%let a = 1;
+data a.data2;
+    set a.data1;
+run;
+
+*inline comment;
+*inline comment;
+/*Comment*/
+proc summary data=a.data2 nway; 
+    class a b c;
+    output out=a.data3;
+run;
+%mend;
+proc transpose data=a.data3 out=a.data4;
+    by a;
+run;
+""",
+[
+libname(library=['a'], path='path/to/folder', pointer=None),
+macroVariableDefinition(variable=['a'], value=' 1'),
+dataStep(outputs=[dataObject(library=['a'], dataset=['data2'], options=None)], header='\n    ', inputs=[dataObject(library=['a'], dataset=['data1'], options=None)], body='\n'),
+comment(text='inline comment'),
+comment(text='inline comment'),
+comment(text='Comment'),
+procedure(outputs=dataObject(library=['a'], dataset=['data3'], options=None), inputs=dataObject(library=['a'], dataset=['data2'], options=None), type='summary'),
+macroEnd(text='%mend;'),
+procedure(outputs=dataObject(library=['a'], dataset=['data4'], options=None), inputs=dataObject(library=['a'], dataset=['data3'], options=None), type='transpose')
+])]
+
+
+@pytest.mark.parametrize("case,expected", testcases)
+def test_force_partial_incomplete_marco_parse(case, expected):
+    res = force_partial_parse(fullprogram, case)
+    assert res == expected
