fc0b390aefceb09053f195b4f734074fd0c5e3a6
Ben Corcoran <Ben.Corcoran@officeforstudents.org.uk>
2020-01-17 20:50:32 +0000

    Basic project testcases Fixes #9

--- tests/test_project.py
+++ tests/test_project.py
@@ -2,10 +2,37 @@ import pytest
 import pprint
 
 from sasdocs.project import sasProject
+from sasdocs.program import sasProgram
+
+testcases = ["bad/path"]
+
+@pytest.mark.parametrize("case", testcases)
+def test_failed_project(case):
+    res = sasProject(case)
+    assert res.programs == list()
+    assert hasattr(res, 'name') is False
+    assert hasattr(res, 'nPrograms') is False
+    assert hasattr(res, 'summary') is False
+    assert hasattr(res, 'objects') is False
 
 
+testcases = [
+    ("./tests/samples",{'programs':[sasProgram('./tests/samples/macro_1.sas'), 
+                                    sasProgram('./tests/samples/macro_2.sas'), 
+                                    sasProgram('./tests/samples/simple_1.sas')],
+                        'names':['macro_1', 'macro_2', 'simple_1']}
+    )
+]
 
-# for x in prj.get_objects():
-#     print(x)
+@pytest.mark.parametrize("case,expected", testcases)
+def test_project_programs(case,expected):
+    res = sasProject(case)
+    assert list(map(lambda x: x.__dict__, res.programs)) == list(map(lambda x: x.__dict__, expected['programs']))
+    assert [prg.name for prg in res.programs] == expected['names']
 
-# Include tests
+
+@pytest.mark.parametrize("case,expected", testcases)
+def test_project_get_objects(case,expected):
+    res = sasProject(case)
+    allObjects = [x for x in res.get_objects()]
+    assert allObjects == [obj for prg in expected['programs'] for obj in prg.get_objects()]
