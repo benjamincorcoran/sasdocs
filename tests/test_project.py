13aa1e1e43981676fa9047e4d53193882ae0be9d
Ben Corcoran <Ben.Corcoran@officeforstudents.org.uk>
2020-01-17 19:36:58 +0000

    Update tests to ref

--- tests/test_project.py
+++ tests/test_project.py
@@ -4,7 +4,7 @@ import pprint
 
 from sasdocs.project import sasProject, sasProgram
 
-# test_project = sasProject(r'S:\HESA AP Student\2017-18\Data checking tool\Code\Live')
-# test_project.generate_documentation(outputDirectory='W:\docs')
+test_project = sasProject(r'S:\HESA AP Student\2017-18\Data checking tool\Code\Live')
+test_project.generate_documentation(outputDirectory='W:\docs')
 
-prg = sasProgram(r'\\hefce\ASDPROJECTS\DataImports\SASCodeLibrary\DILoadingCodeUtilities\Testgo_module.sas')
+# prg = sasProgram(r'\\hefce\ASDPROJECTS\DataImports\SASCodeLibrary\DILoadingCodeUtilities\Testgo_module.sas')
