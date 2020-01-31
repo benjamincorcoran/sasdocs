6f7516e5d21de257d411aa41fc98d9506bba737f
Ben Corcoran <Ben.Corcoran@officeforstudents.org.uk>
2020-01-31 13:34:40 +0000

    sphinx directive, handles folders and files fixes #50 fixes #51 fixes #52

--- sasdocs/sphinx.py
+++ sasdocs/sphinx.py
@@ -10,6 +10,7 @@ from shutil import copyfile
 
 
 from .program import sasProgram
+from .project import sasProject
 
 __version__ = 0.01
 
@@ -21,15 +22,20 @@ class SASDirective(SphinxDirective):
     final_argument_whitespace = False
 
     def run(self):
-        print('SAS Directive Started.')
         sasfile = self.arguments[0]
         srcpath = os.path.normpath(os.path.join(self.env.srcdir,sasfile))
-        destpath = os.path.normpath(os.path.join(self.env.srcdir,os.path.basename(sasfile)))
 
-        parsedSAS = m2r.convert(sasProgram(srcpath).generate_documentation())
-        
-        self.state_machine.insert_input(parsedSAS.split('\n'), '')
-        return []
+        if os.path.splitext(sasfile)[1].lower() == '.sas':
+            parsedSAS = m2r.convert(sasProgram(srcpath).generate_documentation())
+            self.state_machine.insert_input(parsedSAS.split('\n'), '')
+            return []
+        else:
+            parsedSAS = sasProject(srcpath)
+            for prg in parsedSAS.programs:
+                documentation = m2r.convert(prg.generate_documentation())
+                self.state_machine.insert_input(documentation.split('\n'), '')
+            return []
+
 
 
 class SASParser(rst.Parser):
@@ -39,8 +45,7 @@ class SASParser(rst.Parser):
         parsedSAS = sasProgram(document.current_source)
         mdDocumentation = parsedSAS.generate_documentation()
         rst.Parser.parse(self, m2r.convert(mdDocumentation), document)
-
-        
+   
 
 def setup(app):
     app.add_source_suffix('.sas','sas_program')
